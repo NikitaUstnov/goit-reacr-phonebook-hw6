6512e43b4c7ff9694e76ee62a9bffae60bb8ea25
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-03-07 23:34:08 +0200

    crated

---
+++ build/precache-manifest.14bce08e69c437de08cce911a68528ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd04684aa287ede52c019bd89f6c526",
+    "url": "/goit-react-hw-02-phonebook-rebild/index.html"
+  },
+  {
+    "revision": "edc0579f135b9f441a69",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/css/main.dbc250b7.chunk.css"
+  },
+  {
+    "revision": "970b1855de9eeba157ec",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/2.95b2a285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/2.95b2a285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc0579f135b9f441a69",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/main.6e4876ed.chunk.js"
+  },
+  {
+    "revision": "b8f4d1b5995cc901d223",
+    "url": "/goit-react-hw-02-phonebook-rebild/static/js/runtime-main.dceee536.js"
+  }
+]);
---
+++ src/components/App.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import styles from "./App.module.css";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import ContactForm from "./ContactForm/ContactForm";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import Title from "./Title/Title";
+import Alert from "./Alert/Alert";
+
+export default class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+    error: false,
+  };
+
+  addContact = (task) => {
+    const searchSameName = this.state.contacts
+      .map((cont) => cont.name)
+      .includes(task.name);
+
+    if (searchSameName) {
+      this.setState(() => ({
+        error: true,
+      }));
+
+      setTimeout(
+        () =>
+          this.setState((prevStete) => ({
+            error: (this.state.error = !prevStete),
+          })),
+        500
+      );
+    }
+    // else if (task.name.length === 0) {
+    //   alert("Fields must be filled!");
+    // }
+    else {
+      const contact = {
+        ...task,
+        id: uuidv4(),
+      };
+
+      this.setState((prevState) => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
+  };
+
+  changeFilter = (filter) => {
+    this.setState({ filter });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter((contacts) =>
+      contacts.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  removeContact = (contactId) => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+      };
+    });
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const { filter, contacts, error } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <div>
+        {error ? <Alert title="Already exist" /> : <></>}
+
+        <Title title="Phonebook" />
+
+        <ContactForm onAddContact={this.addContact} />
+        <h2>Contacts</h2>
+        <CSSTransition
+          in={contacts.length >= 1}
+          classNames={styles}
+          timeout={250}
+          unmountOnExit
+        >
+          <Filter value={filter} onChangeFilter={this.changeFilter} />
+        </CSSTransition>
+        <CSSTransition
+          in={visibleContacts.length > 0}
+          classNames={styles}
+          timeout={250}
+          unmountOnExit
+        >
+          <ContactList
+            contacts={visibleContacts}
+            onRemoveContact={this.removeContact}
+          />
+        </CSSTransition>
+      </div>
+    );
+  }
+}
